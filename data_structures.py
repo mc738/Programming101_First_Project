8d82c67f0dd9f44f444603eb74b8f5140ab97cec
Max Clifford <me@mclifford.dev>
2024-02-28 20:16:15 +0000

    Work on data_structures

---
+++ data_structures.py
@@ -0,0 +1,17 @@
+import random
+
+class CardDeck:
+
+
+    def __init__(self, items) -> None:
+        self.all_items = items
+        self.current_items = items
+
+    def pick_random(self):
+        self.
+
+
+    def reset_items(self):
+        self.current_items = self.all_items
+        
+
