594dd867be93a9740fa3b67ef4039055134d186b
Max Clifford <me@mclifford.dev>
2024-02-28 20:25:44 +0000

    Fixed missing returns

--- data_structures.py
+++ data_structures.py
@@ -7,18 +7,18 @@ class CardDeck:
         self.current_items = items
 
     def pick_random(self):
-        random.choice(self.current_items)
+        return random.choice(self.current_items)
 
     def take_random(self):
         item = random.choice(self.current_items)
         self.current_items.remove(item)
-        item
+        return item
 
     def take_next(self):
-        self.current_items.pop(1)
+        return self.current_items.pop(1)
     
     def count(self):
-        len(self.current_items)
+        return len(self.current_items)
 
     def reset_items(self):
         self.current_items = self.all_items
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import card
-
+import data_structures
 
 card.Card("", 2)
+
+cd = data_structures.CardDeck([ 1, 2, 3, 4, 5 ])
+
+print(cd.pick_random())
+print(cd.count())
+print(cd.take_random())
+print(cd.count())
+
 
 print("Hello, World!")
