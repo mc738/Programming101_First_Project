9b1a2f42d1cc82ded7612283bfa78282d06b9281
Max Clifford <me@mclifford.dev>
2024-02-28 20:22:57 +0000

    Work on CardDeck

--- data_structures.py
+++ data_structures.py
@@ -2,14 +2,23 @@ import random
 
 class CardDeck:
 
-
     def __init__(self, items) -> None:
         self.all_items = items
         self.current_items = items
 
     def pick_random(self):
-        self.
+        random.choice(self.current_items)
 
+    def take_random(self):
+        item = random.choice(self.current_items)
+        self.current_items.remove(item)
+        item
+
+    def take_next(self):
+        self.current_items.pop(1)
+    
+    def count(self):
+        len(self.current_items)
 
     def reset_items(self):
         self.current_items = self.all_items
